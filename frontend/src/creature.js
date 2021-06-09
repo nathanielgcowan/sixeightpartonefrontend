f01757a618884e8fe02e5a0a1c6c70d65e2be292
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-09 01:04:02 -0500

    constructor for creature

--- frontend/src/creature.js
+++ frontend/src/creature.js
@@ -1,3 +1,14 @@
 class Creature {
-    
+
+    static allCreatures = []
+
+    constructor(creature){
+        this.id = creature.id
+        this.name = creature.attributes.name
+        this.image = creature.attributes.image
+        this.description = creature.attributes.description
+        this.skills = creature.attributes.skills
+        Creature.allCreatures.push(this)
+        this.renderCreature()
+    }
 }

c4b1b9ea13c391e64e6f90880e3384ed0ca0fe67
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-06-09 01:01:35 -0500

    this is for the index.js and Creature class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,9 @@
-console.log("Hello");
+const creaturesURL = "http://localhost:3000/creatures"
+const skillsURL = "http://localhost:3000/skills"
+const makeACreature = document.getElementById("makeacreature");
+const enterCreatureName = document.getElementsByClassName("creaturename");
+const enterCreatureImage = document.querySelector(".creaturedescription")
+const card = document.querySelector(".card")
+
+makeACreature.addEventListener("submit", Creature.submitCreature)
+Creature.fetchCreatures()
